387ad2ae3f659b8474ddcfb5bfff262de8a61f83
Ramazan KARABULUT <32045100+rmznkrblt@users.noreply.github.com>
2021-01-24 22:59:22 +0300

    Casting
    type casting

--- TypeCasting/Program.cs
+++ TypeCasting/Program.cs
@@ -18,7 +18,17 @@ namespace TypeCasting
             int myInt2 = (int)myDouble;   
 
             Console.WriteLine(myDouble2);
-            Console.WriteLine(myInt2);   
+            Console.WriteLine(myInt2);
+
+            int i = 75;
+            float f = 53.005f;
+            double d = 2345.7652;
+            bool b = true;
+
+            Console.WriteLine(i.ToString());
+            Console.WriteLine(f.ToString());
+            Console.WriteLine(d.ToString());
+            Console.WriteLine(b.ToString());
         }
     }
 }
