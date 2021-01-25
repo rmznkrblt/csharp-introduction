f5cce2e8995c5f6dabd99bcbf955d3c7a7be96f6
Ramazan KARABULUT <32045100+rmznkrblt@users.noreply.github.com>
2021-01-25 22:23:10 +0300

    Strings

---
+++ Strings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
