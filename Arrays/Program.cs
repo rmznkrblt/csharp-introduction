600c20a483f8fb101cc45d891291b0e7ab1fa787
Ramazan KARABULUT <32045100+rmznkrblt@users.noreply.github.com>
2021-02-06 14:03:06 +0300

    Update Program.cs
    Arrays added

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,7 +6,35 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] students = new string[3]{ "Ramazan", "Alper", "Cigdem" };
+            students = new string[4] {"", "", "", ""};
+            string[] students2 = {"Ramazan", "Alper", "Cigdem" };
+            //students2[3] = "Ahmet";
+
+            //foreach (var student in students2)
+            //{
+            //    Console.WriteLine(student);
+            //}
+
+            string[,] regions=new string[5,3]
+            {
+                {"İstanbul","İzmit","Balıkesir" },
+                {"Ankara","Konya","Kırıkkale" },
+                {"Antalya","Adana","Mersin" },
+                {"Rize","Trabzon","Samsun" },
+                {"İzmir","Muğla","Manisa" }
+            };
+
+            for (int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i,j]);
+                }
+                Console.WriteLine("*********");
+            }
+
+            Console.ReadLine();
         }
     }
 }
